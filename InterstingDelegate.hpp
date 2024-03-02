3701e1a7c929bece1b0e4efed353402829b7378f
Zoe <2625792753@qq.com>
2024-03-03 00:50:16 +0800

    FunctionWrapper

--- InterstingDelegate.hpp
+++ InterstingDelegate.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <mutex>
 #include <memory>
+#include <functional>
 
 template <typename ReturnType, typename... Args>
 class Callable;
@@ -15,42 +16,21 @@ public:
     virtual ~Callable() {}
 };
 
-template <typename ClassName, typename ReturnType, typename... Args>
-class MemberFunction;
-
-template <typename ClassName, typename ReturnType, typename... Args>
-class MemberFunction<ClassName, ReturnType(Args...)> : public Callable<ReturnType(Args...)>
-{
-    using func_ptr = ReturnType (ClassName::*)(Args...);
-
-private:
-    ClassName *instance_p;
-    func_ptr function_p;
-
-public:
-    MemberFunction(ClassName *_insp, func_ptr _fp) : instance_p(_insp), function_p(_fp) {}
-    ReturnType Execute(Args... args)
-    {
-        return (instance_p->*function_p)(args...);
-    }
-};
+template <typename FunctionType>
+class FunctionWrapper;
 
 template <typename ReturnType, typename... Args>
-class StaticFunction;
-
-template <typename ReturnType, typename... Args>
-class StaticFunction<ReturnType(Args...)> : public Callable<ReturnType(Args...)>
+class FunctionWrapper<ReturnType(Args...)> : public Callable<ReturnType(Args...)>
 {
-    using func_ptr = ReturnType (*)(Args...);
-
 private:
-    func_ptr function_p;
+    std::function<ReturnType(Args...)> function;
 
 public:
-    StaticFunction(func_ptr _fp) : function_p(_fp) {}
-    ReturnType Execute(Args... args)
+    FunctionWrapper(const std::function<ReturnType(Args...)>& func) : function(func) {}
+
+    ReturnType Execute(Args... args) override
     {
-        return function_p(args...);
+        return function(args...);
     }
 };
 
@@ -63,81 +43,63 @@ class Delegate<ReturnType(Args...)>
     using callable_ptr = std::shared_ptr<Callable<ReturnType(Args...)>>;
 
 private:
-#include <mutex>
-
     std::mutex mtx;
-
     std::vector<callable_ptr> callable_ptrs;
 
-    Delegate<ReturnType(Args...)> &operator+=(const callable_ptr &f) noexcept
+    Delegate<ReturnType(Args...)>& operator+=(const callable_ptr& f) noexcept
     {
         std::lock_guard<std::mutex> lock(mtx);
         callable_ptrs.push_back(f);
         return *this;
     }
 
-    void operator-=(const callable_ptr &f) noexcept
+    void operator-=(const callable_ptr& f) noexcept
     {
         std::lock_guard<std::mutex> lock(mtx);
-        for (typename ::std::vector<callable_ptr>::reverse_iterator it = callable_ptrs.rbegin(); it < callable_ptrs.rend(); ++it)
+        for (auto it = callable_ptrs.rbegin(); it != callable_ptrs.rend(); ++it)
         {
             if (*it == f)
+            {
                 callable_ptrs.erase((++it).base());
-            return;
+                return;
+            }
         }
     }
-
-    void ClearCallablePtrs() { callable_ptrs.clear(); }
 
 public:
     Delegate() {}
 
-    Delegate(ReturnType (*_fp)(Args...))
+    Delegate(const std::function<ReturnType(Args...)>& func)
     {
-        callable_ptrs.push_back(std::make_shared<StaticFunction<ReturnType(Args...)>>(_fp));
+        callable_ptrs.push_back(std::make_shared<FunctionWrapper<ReturnType(Args...)>>(func));
     }
 
-    template <typename ClassName>
-    Delegate(ClassName *_instance_p, ReturnType (ClassName::*_fp)(Args...))
+    template <typename FunctionType>
+    Delegate(const FunctionType& func)
     {
-        callable_ptrs.push_back(std::make_shared<MemberFunction<ClassName, ReturnType(Args...)>>(_instance_p, _fp));
+        callable_ptrs.push_back(std::make_shared<FunctionWrapper<ReturnType(Args...)>>(std::function<ReturnType(Args...)>(func)));
     }
 
-    const std::vector<callable_ptr> &GetCallablePtrs() const { return callable_ptrs; }
+    const std::vector<callable_ptr>& GetCallablePtrs() const { return callable_ptrs; }
 
-    // It might NOT work if i just simply copy the callable_ptrs
-    Delegate(const Delegate &_other) = delete;
-
-    Delegate(const Delegate &&_other) : callable_ptrs(_other.GetCallablePtrs())
+    Delegate<ReturnType(Args...)>& operator+=(const Delegate<ReturnType(Args...)>& function)
     {
-        _other.ClearCallablePtrs();
-    }
-
-    ~Delegate()
-    {
-        callable_ptrs.clear();
-    }
-
-    Delegate<ReturnType(Args...)> &operator+=(const Delegate<ReturnType(Args...)> &function)
-    {
-        for (callable_ptr f : function.GetCallablePtrs())
+        for (const auto& f : function.GetCallablePtrs())
             *this += f;
         return *this;
     }
 
-    void operator-=(const Delegate<ReturnType(Args...)> &function)
+    void operator-=(const Delegate<ReturnType(Args...)>& function)
     {
-        for (typename ::std::vector<callable_ptr>::const_iterator it = function.GetCallablePtrs().begin(); it < function.GetCallablePtrs().end(); ++it)
-        {
-            *this -= *it;
-        }
+        for (const auto& f : function.GetCallablePtrs())
+            *this -= f;
     }
 
     std::vector<ReturnType> Execute(Args... args) noexcept
     {
         std::lock_guard<std::mutex> lock(mtx);
         std::vector<ReturnType> results;
-        for (callable_ptr f : callable_ptrs)
+        for (const auto& f : callable_ptrs)
             results.push_back(f->Execute(args...));
 
         return results;
@@ -157,75 +119,61 @@ class Delegate<void(Args...)>
 
 private:
     std::mutex mtx;
-
     std::vector<callable_ptr> callable_ptrs;
 
-    Delegate<void(Args...)> &operator+=(const callable_ptr &f) noexcept
+    Delegate<void(Args...)>& operator+=(const callable_ptr& f) noexcept
     {
         std::lock_guard<std::mutex> lock(mtx);
         callable_ptrs.push_back(f);
         return *this;
     }
 
-    void operator-=(const callable_ptr &f) noexcept
+    void operator-=(const callable_ptr& f) noexcept
     {
         std::lock_guard<std::mutex> lock(mtx);
-        for (typename ::std::vector<callable_ptr>::reverse_iterator it = callable_ptrs.rbegin(); it < callable_ptrs.rend(); ++it)
+        for (auto it = callable_ptrs.rbegin(); it != callable_ptrs.rend(); ++it)
         {
             if (*it == f)
+            {
                 callable_ptrs.erase((++it).base());
-            return;
+                return;
+            }
         }
     }
 
 public:
     Delegate() {}
 
-    Delegate(void (*_fp)(Args...))
+    Delegate(const std::function<void(Args...)>& func)
     {
-        callable_ptrs.push_back(std::make_shared<StaticFunction<void(Args...)>>(_fp));
+        callable_ptrs.push_back(std::make_shared<FunctionWrapper<void(Args...)>>(func));
     }
 
-    template <typename ClassName>
-    Delegate(ClassName *_instance_p, void (ClassName::*_fp)(Args...))
+    template <typename FunctionType>
+    Delegate(const FunctionType& func)
     {
-        callable_ptrs.push_back(std::make_shared<MemberFunction<ClassName, void(Args...)>>(_instance_p, _fp));
+        callable_ptrs.push_back(std::make_shared<FunctionWrapper<void(Args...)>>(std::function<void(Args...)>(func)));
     }
 
-    const std::vector<callable_ptr> &GetCallablePtrs() const { return callable_ptrs; }
+    const std::vector<callable_ptr>& GetCallablePtrs() const { return callable_ptrs; }
 
-    // It might NOT work if i just simply copy the callable_ptrs
-    Delegate(const Delegate &_other) = delete;
-
-    Delegate(const Delegate &&_other) : callable_ptrs(_other.GetCallablePtrs())
+    Delegate<void(Args...)>& operator+=(const Delegate<void(Args...)>& function)
     {
-        _other.ClearCallablePtrs();
-    }
-
-    ~Delegate()
-    {
-        callable_ptrs.clear();
-    }
-
-    Delegate<void(Args...)> &operator+=(Delegate<void(Args...)> &function)
-    {
-        for (callable_ptr f : function.GetCallablePtrs())
+        for (const auto& f : function.GetCallablePtrs())
             *this += f;
         return *this;
     }
 
-    void operator-=(Delegate<void(Args...)> &function)
+    void operator-=(const Delegate<void(Args...)>& function)
     {
-        for (typename ::std::vector<callable_ptr>::const_iterator it = function.GetCallablePtrs().begin(); it < function.GetCallablePtrs().end(); ++it)
-        {
-            *this -= *it;
-        }
+        for (const auto& f : function.GetCallablePtrs())
+            *this -= f;
     }
 
     void Execute(Args... args) noexcept
     {
         std::lock_guard<std::mutex> lock(mtx);
-        for (callable_ptr f : callable_ptrs)
+        for (const auto& f : callable_ptrs)
             f->Execute(args...);
     }
 
